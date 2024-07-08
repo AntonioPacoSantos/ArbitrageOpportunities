cbdf4a5480ab5cacf020121f31dbb94f67f97267
AntonioPacoSantos <antoniopacosantos@gmail.com>
2024-07-07 21:58:30 -0300

    organizing functions

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import math 
 import pyRofex as pr
 from datetime import date 
+import os 
 
 
 def compute_rate(symbol,bid_price,spots):
@@ -15,3 +16,30 @@ def parse_maturity_date_and_compute_tValue(maturity_date):
     maturity_parsed_date = date(int(maturity_date[0:4]), int(maturity_date[4:6]), int(maturity_date[6:8]))
     tValue = (maturity_parsed_date - current_date).days / 365
     return tValue    
+
+
+def get_credentials():
+    if not os.path.exists('config.txt'):
+        config_file = open('config.txt', "w")
+        print("No existe el archivo de configuración.")
+        user = input('Ingrese su usario:') 
+        password = input('Ingrese su contraseña:')
+        account = input('Ingrese su cuenta:')
+        config_file.write(f"user={user}\n",)
+        config_file.write(f"password={password}\n")
+        config_file.write(f"account={account}")
+        config_file.close()
+        credentials = {
+            'user': user,
+            'password': password,
+            'account': account
+        }
+    else:
+        config_file = open('config.txt', "r")
+        credentials = {}
+        for line in config_file:
+            key, value = line.split('=')
+            value = value.strip()
+            credentials[key] = value
+    return credentials
+    
